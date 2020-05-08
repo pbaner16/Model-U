b3ca942ebd7952c7513d11fdcd519a82e3c911f8
pbaner16 <pbanerje@mail.sfsu.edu>
2020-05-08 01:21:59 -0700

    initial commits

---
+++ PB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PB
+//
+//  Created by Ida Hui on 5/8/20.
+//  Copyright © 2020 Ida Hui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var field1: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        button1.layer.cornerRadius = 25
+        
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
